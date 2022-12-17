83753ec6c2fa1a20f7896897486b9c6bf130aebc
Kien <huatrungkien126@gmail.com>
2022-12-17 14:00:56 +0700

    update

--- quanlysinhvien_java/src/quanlysinhvien_java/giaodien.java
+++ quanlysinhvien_java/src/quanlysinhvien_java/giaodien.java
@@ -63,7 +63,6 @@ public class giaodien extends JFrame {
 		scrollPane.setBounds(600, 150, 867, 307);
 		contentPane.add(scrollPane);
 		
-		
 		///HEADER
 		JLabel lblNewLabel = new JLabel("QUẢN LÝ SINH VIÊN");
 		Font fontLb = new Font(lblNewLabel.getFont().getName(), Font.PLAIN, 20);
@@ -135,6 +134,5 @@ public class giaodien extends JFrame {
 				}
 			}
 		});
-
-}
+	}
 }
