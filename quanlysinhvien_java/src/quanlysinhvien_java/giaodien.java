e0e4aa5490c910b4fb43446ae41b09ea07b3e153
Kien <huatrungkien126@gmail.com>
2022-12-17 19:22:19 +0700

    change

--- quanlysinhvien_java/src/quanlysinhvien_java/giaodien.java
+++ quanlysinhvien_java/src/quanlysinhvien_java/giaodien.java
@@ -16,8 +16,6 @@ import java.awt.SystemColor;
 import java.awt.Color;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
